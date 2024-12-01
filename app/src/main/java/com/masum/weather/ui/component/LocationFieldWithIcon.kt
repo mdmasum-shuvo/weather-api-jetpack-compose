5efa42e99bd63ce6a7bea00b8bc3f159fa45c6a0
masum <mdmasum.shuvo.cse@gmail.com>
2024-12-01 23:58:46 +0600

    commit all

--- app/src/main/java/com/masum/weather/ui/component/LocationFieldWithIcon.kt
+++ app/src/main/java/com/masum/weather/ui/component/LocationFieldWithIcon.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,7 +32,6 @@ import com.masum.weather.ui.theme.Purple40
 import com.masum.weather.ui.theme.light_gray
 import com.masum.weather.ui.theme.text_gray
 import com.masum.weather.ui.theme.white_color
-
 
 
 @Composable
